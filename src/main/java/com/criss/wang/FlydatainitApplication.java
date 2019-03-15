7887cfeebab37fb37dba30b2fc18c2e6a7d82cfc
wangqiubao <wangqiubao@u-care.net>
2019-03-15 10:51:13 +0800

    添加逻辑

--- src/main/java/com/criss/wang/FlydatainitApplication.java
+++ src/main/java/com/criss/wang/FlydatainitApplication.java
@@ -12,6 +12,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
@@ -19,15 +22,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.criss.wang.entity.Geometry;
+import com.criss.wang.entity.UavTrack;
+import com.criss.wang.entity.UavTrackProps;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.cc.commons.MathUtils;
 import net.cc.commons.StringUtils;
 import net.cc.luffy.entity.proto.UavFlyDataBatchProto;
 import net.cc.luffy.entity.proto.UavFlyDataProto;
-import com.criss.wang.entity.UavTrack;
-import com.criss.wang.entity.UavTrackProps;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class FlydatainitApplication {
@@ -44,7 +47,7 @@ public class FlydatainitApplication {
 
 		if (args.length < 5) {
 			System.out.println(args[0]);
-			System.out.println("参数不完整");
+			System.out.println("参数不完整, 程序退出");
 			System.exit(-1);
 		}
 
@@ -80,6 +83,8 @@ public class FlydatainitApplication {
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		PreparedStatement flyDataPs = null;
 		ResultSet flyDataRs = null;
+		// buerId:iso
+		Map<String, Integer> isoMark = null;
 		try {
 			// 查询fly_data_info总数据量
 			flyDataPs = dbConn.prepareStatement("SELECT COUNT(*) FROM FLY_DATA_INFO");
@@ -96,13 +101,19 @@ public class FlydatainitApplication {
 				flyDataRs = flyDataPs.executeQuery();
 				while (flyDataRs.next()) {
 					UavTrack jsonTrack = objectMapper.readValue(flyDataRs.getString(3), UavTrack.class);
+					// 获取buyerId
+					String buyerId = getBuyerIdByDeviceId(flyDataRs.getString(2), dbConn);
+					if (buyerId != null) {
+						// 获取buyerId对应的隔离级别 buyerId:iso
+						isoMark = getIsoMarkByBuyerId(buyerId, dbConn);
+					}
 					// 拆点入Hbase
-					convertTrack2ProtoBytes(flyDataRs.getString(2), jsonTrack);
+					convertTrack2ProtoBytes(isoMark, flyDataRs.getString(2), jsonTrack);
 				}
 				row += 10;
 				if (row < Integer.valueOf(rowCount))
 					System.out.println("当前row: " + row + ", rowCount: " + rowCount + ",当前已完成进度： "
-							+ (MathUtils.div(Double.valueOf(row), Double.valueOf(rowCount), 2, RoundingMode.HALF_UP)
+							+ (MathUtils.div(Double.valueOf(row), Double.valueOf(rowCount), 4, RoundingMode.HALF_UP)
 									* 100)
 							+ "%");
 			}
@@ -120,13 +131,81 @@ public class FlydatainitApplication {
 		}
 	}
 
-	public static void convertTrack2ProtoBytes(String flyId, UavTrack jsonTrack) {
+	private static Map<String, Integer> getIsoMarkByBuyerId(String buyerId, Connection dbConn) {
+		Map<String, Integer> isoMark = new HashMap<>();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = dbConn.prepareStatement("SELECT BUYER_NAME FROM UCARE_AGENTS_BUYER_INFO WHERE BUYER_ID = ?");
+			ps.setString(1, buyerId);
+			rs = ps.executeQuery();
+			if (!rs.next()) {
+				return null;
+			}
+			String buyerName = rs.getString(1);
+			if (buyerName.contains("翼比特") || buyerName.contains("中科院")) {
+				isoMark.put(buyerId, 2);
+			} else if (buyerName.contains("中船")) {
+				isoMark.put(buyerId, 1);
+			} else {
+				isoMark = null;
+			}
+			return isoMark;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static String getBuyerIdByDeviceId(String deviceId, Connection dbConn) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String flyId = null;
+		flyId = deviceId;
+		if (!deviceId.startsWith("A")) {
+			try {
+				ps = dbConn.prepareStatement(
+						"SELECT FLY_ID FROM DEVICE_MAPPING_INFO WHERE DEVICE_ID = ? AND FLY_DATA_TYPE = 'Bind'");
+				ps.setString(1, deviceId);
+				rs = ps.executeQuery();
+				if (!rs.next()) {
+					return null;
+				}
+				// 获取FlyId
+				flyId = rs.getString(1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			// 如果FlyId有效则根据其查询buyerId
+			if (!StringUtils.isEmpty(flyId)) {
+				ps = dbConn.prepareStatement("SELECT BUYER_ID FROM UCARE_AGENTS_BUYER_CONFIG_INFO WHERE AGENT_ID = ?");
+				ps.setString(1, flyId);
+				rs = ps.executeQuery();
+				if (!rs.next())
+					return null;
+				// 获取buyerId
+				String buyerId = rs.getString(1);
+				if (!StringUtils.isEmpty(buyerId)) {
+					return buyerId;
+				}
+			}
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void convertTrack2ProtoBytes(Map<String, Integer> isoMark, String flyId, UavTrack jsonTrack) {
 		UavFlyDataBatchProto.UavFlyDataBatch.Builder uavFlyDataBatch = UavFlyDataBatchProto.UavFlyDataBatch
 				.newBuilder();
 		Geometry geo = jsonTrack.getGeometry();
 		UavTrackProps props = jsonTrack.getProperties();
 
-		//
+		// 计数
 		int sendCount = 1;
 
 		for (int i = 0; i < geo.getCoordinates().length; i++) {
@@ -175,7 +254,7 @@ public class FlydatainitApplication {
 
 			// 如果batch中包含了2000个点，则将这2000个点发出去
 			if (sendCount % 2000 == 0) {
-				sendFlyData(uavFlyDataBatch);
+				sendFlyData(isoMark, uavFlyDataBatch);
 				uavFlyDataBatch.clear();
 			} else {
 				uavFlyDataBatch.addFlyData(uavFlyData.build());
@@ -186,11 +265,20 @@ public class FlydatainitApplication {
 
 		// 如果有剩余数据也需要发出去
 		if (uavFlyDataBatch.build().getFlyDataCount() > 0) {
-			sendFlyData(uavFlyDataBatch);
+			sendFlyData(isoMark, uavFlyDataBatch);
 		}
 	}
 
-	public static void sendFlyData(UavFlyDataBatchProto.UavFlyDataBatch.Builder uavFlyDataBatch) {
+	public static void sendFlyData(Map<String, Integer> isoMark,
+			UavFlyDataBatchProto.UavFlyDataBatch.Builder uavFlyDataBatch) {
+		String iso2 = null;
+		if (isoMark != null) {
+			Optional<String> buyerId = isoMark.keySet().stream().findFirst();
+			if (buyerId.isPresent()) {
+				iso2 = "&isoMark=2&buyerId=" + buyerId.get();
+			}
+		}
+
 		try {
 			System.out.println("uploadTime为空数量：" + count);
 			count = 0; // count清零
@@ -199,6 +287,9 @@ public class FlydatainitApplication {
 			// 调用批量发送接口发送数据
 			// String s = "http://192.168.3.37:8100/flydata/uav/batch?toRealtime=true";
 			String s = "http://" + hbaseUrl + "/flydata/uav/batch?toRealtime=true";
+			if (iso2 != null) {
+				s += iso2;
+			}
 			URL url = new URL(s);
 			HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
 			urlc.setDoInput(true);
